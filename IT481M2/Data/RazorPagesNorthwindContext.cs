369c495944876512767300387457dd23930da222
Kory Jacobs <kory.jacobs@pb.com>
2020-12-05 18:00:59 -0800

    Add project files.

---
+++ IT481M2/Data/RazorPagesNorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IT481M2.Data
+{
+    public class RazorPagesNorthwindContext : DbContext
+    {
+        public RazorPagesNorthwindContext(
+            DbContextOptions<RazorPagesNorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Customer> Customers { get; set; }
+    }
+}
---
+++ IT481M2/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using IT481M2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace IT481M2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly IT481M2.Data.RazorPagesNorthwindContext _context;
+
+        public IndexModel(IT481M2.Data.RazorPagesNorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Customer { get; set; }
+
+        public IEnumerable<string> CustomerLastNames { get; set; }
+
+        public int Count { get; set; }
+
+
+
+        public async Task<IActionResult> OnGetAsync(string? id)
+        {
+
+            Count = await _context.Customers.CountAsync();
+
+            CustomerLastNames = await _context.Customers.Select(customer => customer.ContactName).ToListAsync();
+
+            return Page();
+        }
+    }
+}
